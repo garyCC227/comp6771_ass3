bc2fff0f58087a3cd97163376a9b3f41567ad702
garyCC227 <z5163479@ad.unsw.edu.au>
2019-07-22 23:38:12 +1000

    data structure set up, with all custom comparator

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -1,15 +1,47 @@
 #ifndef ASSIGNMENTS_DG_GRAPH_H_
 #define ASSIGNMENTS_DG_GRAPH_H_
 
+#include <map>
+#include <set>
+#include <memory>
+#include <utility>
+
 namespace gdwg {
 
-template <typename N, typename E>
-class Graph {
- public:
-  class const_iterator {};
+  template<typename N>
+  struct CompareByNode {
+    bool operator()(const N& lhs, const N& rhs) {
+      return lhs->value < rhs->value;
+    }
+  };// comparator for Node
 
- private:
-};
+  template <typename EP>
+  struct CompareByEdgePair{
+      bool operator()(const EP& lhs, const EP& rhs){
+        return (std::get<0>(lhs).lock()->value < std::get<0>(rhs).lock()->value);
+      }
+  };
+
+  template<typename N, typename E>
+  class Graph {
+      // Node struct
+    private:
+      struct Node {
+          Node(N value): value{value}{};
+          N value;
+      };
+
+    public:
+      //type alias
+      using EdgePair = std::pair<std::weak_ptr<Node>, E>;
+      using EdgeSet = std::set<EdgePair, CompareByEdgePair<EdgePair>>;
+      using NodePtr = std::shared_ptr<Node>;
+
+      class const_iterator {};
+
+    private:
+      std::map<NodePtr, EdgeSet, CompareByNode<NodePtr>> nodes_;
+  };
 
 }  // namespace gdwg
 
